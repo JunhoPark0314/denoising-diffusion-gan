2453b5d6f3f8c2f74612d627a7a350ca0d031ff5
junhopark <pjh3974@gmail.com>
2022-09-12 16:08:39 +0000

    init changes

--- train_ddgan.py
+++ train_ddgan.py
@@ -19,6 +19,7 @@ import torchvision
 
 import torchvision.transforms as transforms
 from torchvision.datasets import CIFAR10
+import yaml
 from datasets_prep.lsun import LSUN
 from datasets_prep.stackmnist_data import StackedMNIST, _data_transforms_stacked_mnist
 from datasets_prep.lmdb_datasets import LMDBDataset
@@ -27,6 +28,38 @@ from datasets_prep.lmdb_datasets import LMDBDataset
 from torch.multiprocessing import Process
 import torch.distributed as dist
 import shutil
+
+def get_beta_schedule(beta_schedule, *, beta_start, beta_end, num_diffusion_timesteps):
+    def sigmoid(x):
+        return 1 / (np.exp(-x) + 1)
+
+    if beta_schedule == "quad":
+        betas = (
+            np.linspace(
+                beta_start ** 0.5,
+                beta_end ** 0.5,
+                num_diffusion_timesteps,
+                dtype=np.float64,
+            )
+            ** 2
+        )
+    elif beta_schedule == "linear":
+        betas = np.linspace(
+            beta_start, beta_end, num_diffusion_timesteps, dtype=np.float64
+        )
+    elif beta_schedule == "const":
+        betas = beta_end * np.ones(num_diffusion_timesteps, dtype=np.float64)
+    elif beta_schedule == "jsd":  # 1/T, 1/(T-1), 1/(T-2), ..., 1
+        betas = 1.0 / np.linspace(
+            num_diffusion_timesteps, 1, num_diffusion_timesteps, dtype=np.float64
+        )
+    elif beta_schedule == "sigmoid":
+        betas = np.linspace(-6, 6, num_diffusion_timesteps)
+        betas = sigmoid(betas) * (beta_end - beta_start) + beta_start
+    else:
+        raise NotImplementedError(beta_schedule)
+    assert betas.shape == (num_diffusion_timesteps,)
+    return betas
 
 def copy_source(file, output_dir):
     shutil.copyfile(file, os.path.join(output_dir, os.path.basename(file)))
@@ -53,7 +86,7 @@ def extract(input, t, shape):
     return out
 
 def get_time_schedule(args, device):
-    n_timestep = args.num_timesteps
+    n_timestep = args.num_timesteps // args.step_size
     eps_small = 1e-3
     t = np.arange(0, n_timestep + 1, dtype=np.float64)
     t = t / n_timestep
@@ -85,113 +118,102 @@ def get_sigma_schedule(args, device):
     return sigmas, a_s, betas
 
 class Diffusion_Coefficients():
-    def __init__(self, args, device):
+    def __init__(self, config, device):
                 
-        self.sigmas, self.a_s, _ = get_sigma_schedule(args, device=device)
-        self.a_s_cum = np.cumprod(self.a_s.cpu())
-        self.sigmas_cum = np.sqrt(1 - self.a_s_cum ** 2)
-        self.a_s_prev = self.a_s.clone()
-        self.a_s_prev[-1] = 1
-        
-        self.a_s_cum = self.a_s_cum.to(device)
-        self.sigmas_cum = self.sigmas_cum.to(device)
-        self.a_s_prev = self.a_s_prev.to(device)
+        betas = get_beta_schedule(
+            beta_schedule=config.diffusion.beta_schedule,
+            beta_start=config.diffusion.beta_start,
+            beta_end=config.diffusion.beta_end,
+            num_diffusion_timesteps=config.diffusion.num_diffusion_timesteps,
+        )
+        betas = self.betas = torch.from_numpy(betas).float().to(device)
+        self.num_timesteps = args.num_timesteps
+        self.step_size = args.step_size
     
-def q_sample(coeff, x_start, t, *, noise=None):
+    def compute_alpha(self, t):
+        beta = torch.cat([torch.zeros(1).to(self.betas.device), self.betas], dim=0)
+        a = (1 - beta).cumprod(dim=0).index_select(0, t+1).view(-1, 1, 1, 1)
+        return a
+    
+def q_sample(coeff, x_start, t, netTrg, *, noise=None):
     """
     Diffuse the data (t == 0 means diffused for t step)
     """
     if noise is None:
       noise = torch.randn_like(x_start)
-      
-    x_t = extract(coeff.a_s_cum, t, x_start.shape) * x_start + \
-          extract(coeff.sigmas_cum, t, x_start.shape) * noise
     
-    return x_t
+    a = coeff.compute_alpha(t)
+      
+    x_t = a.sqrt() * x_start + (1 - a).sqrt() * noise
+    eps_trg = netTrg(x_t, t)
+    x_0 = (x_t - (1 - a).sqrt() * eps_trg) / a.sqrt()
+    
+    return x_0
 
-def q_sample_pairs(coeff, x_start, t):
+def q_sample_pairs(coeff, x_start, t, netTrg):
     """
     Generate a pair of disturbed images for training
     :param x_start: x_0
     :param t: time step t
     :return: x_t, x_{t+1}
     """
-    noise = torch.randn_like(x_start)
-    x_t = q_sample(coeff, x_start, t)
-    x_t_plus_one = extract(coeff.a_s, t+1, x_start.shape) * x_t + \
-                   extract(coeff.sigmas, t+1, x_start.shape) * noise
+    # noise = torch.randn_like(x_start)
+    x_t = q_sample(coeff, x_start, t, netTrg)
+    prev_t = (t + coeff.step_size).clip(max=coeff.num_timesteps-1)
+    x_t_plus_one = q_sample(coeff, x_start, prev_t, netTrg)
+    # x_t_plus_one = extract(coeff.a_s, prev_t, x_start.shape) * x_t + \
+    #                extract(coeff.sigmas, prev_t, x_start.shape) * noise
     
-    return x_t, x_t_plus_one
-#%% posterior sampling
-class Posterior_Coefficients():
-    def __init__(self, args, device):
-        
-        _, _, self.betas = get_sigma_schedule(args, device=device)
-        
-        #we don't need the zeros
-        self.betas = self.betas.type(torch.float32)[1:]
-        
-        self.alphas = 1 - self.betas
-        self.alphas_cumprod = torch.cumprod(self.alphas, 0)
-        self.alphas_cumprod_prev = torch.cat(
-                                    (torch.tensor([1.], dtype=torch.float32,device=device), self.alphas_cumprod[:-1]), 0
-                                        )               
-        self.posterior_variance = self.betas * (1 - self.alphas_cumprod_prev) / (1 - self.alphas_cumprod)
-        
-        self.sqrt_alphas_cumprod = torch.sqrt(self.alphas_cumprod)
-        self.sqrt_recip_alphas_cumprod = torch.rsqrt(self.alphas_cumprod)
-        self.sqrt_recipm1_alphas_cumprod = torch.sqrt(1 / self.alphas_cumprod - 1)
-        
-        self.posterior_mean_coef1 = (self.betas * torch.sqrt(self.alphas_cumprod_prev) / (1 - self.alphas_cumprod))
-        self.posterior_mean_coef2 = ((1 - self.alphas_cumprod_prev) * torch.sqrt(self.alphas) / (1 - self.alphas_cumprod))
-        
-        self.posterior_log_variance_clipped = torch.log(self.posterior_variance.clamp(min=1e-20))
-        
-def sample_posterior(coefficients, x_0,x_t, t):
-    
-    def q_posterior(x_0, x_t, t):
-        mean = (
-            extract(coefficients.posterior_mean_coef1, t, x_t.shape) * x_0
-            + extract(coefficients.posterior_mean_coef2, t, x_t.shape) * x_t
-        )
-        var = extract(coefficients.posterior_variance, t, x_t.shape)
-        log_var_clipped = extract(coefficients.posterior_log_variance_clipped, t, x_t.shape)
-        return mean, var, log_var_clipped
-    
-  
-    def p_sample(x_0, x_t, t):
-        mean, _, log_var = q_posterior(x_0, x_t, t)
-        
-        noise = torch.randn_like(x_t)
-        
-        nonzero_mask = (1 - (t == 0).type(torch.float32))
+    return x_t.detach(), x_t_plus_one.detach()
 
-        return mean + nonzero_mask[:,None,None,None] * torch.exp(0.5 * log_var) * noise
-            
-    sample_x_pos = p_sample(x_0, x_t, t)
-    
-    return sample_x_pos
+def sample_posterior(coeff, x_0, x_t, t, next_t, **kwargs):
+    at = coeff.compute_alpha(t)
+    at_next = coeff.compute_alpha(next_t)
+    et = (x_t - x_0 * at.sqrt()) / (1 - at).sqrt()
 
-def sample_from_model(coefficients, generator, n_time, x_init, T, opt):
+    c1 = (
+        kwargs.get("eta", 0) * ((1 - at / at_next) * (1 - at_next) / (1 - at)).sqrt()
+    )
+    c2 = ((1 - at_next) - c1 ** 2).sqrt()
+    xt_next = at_next.sqrt() * x_0 + c1 * torch.randn_like(x_0) + c2 * et
+    
+    return xt_next
+
+def sample_from_model(coeff, generator, n_time, x_init, T, opt, netTrg):
     x = x_init
     with torch.no_grad():
-        for i in reversed(range(n_time)):
-            t = torch.full((x.size(0),), i, dtype=torch.int64).to(x.device)
+
+        t = torch.full((x.size(0),), 999, dtype=torch.int64).to(x.device)
+        x = netTrg(x, t)
+        for i in reversed(range(1, n_time)):
+            t = torch.full((x.size(0),), i * coeff.step_size, dtype=torch.int64).to(x.device)
           
-            t_time = t
+            # t_time = t
+            # next_time = (t - coeff.step_size).clip(min=0)
             latent_z = torch.randn(x.size(0), opt.nz, device=x.device)
-            x_0 = generator(x, t_time, latent_z)
-            x_new = sample_posterior(coefficients, x_0, x, t)
-            x = x_new.detach()
+            x = generator(x, t, latent_z)
+            # x_new = sample_posterior(coeff, x_0, x, t)
+            # x = x_new.detach()
         
     return x
+
+def dict2namespace(config):
+    namespace = argparse.Namespace()
+    for key, value in config.items():
+        if isinstance(value, dict):
+            new_value = dict2namespace(value)
+        else:
+            new_value = value
+        setattr(namespace, key, new_value)
+    return namespace
 
 #%%
 def train(rank, gpu, args):
     from score_sde.models.discriminator import Discriminator_small, Discriminator_large
     from score_sde.models.ncsnpp_generator_adagn import NCSNpp
+    from ddim.models.diffusion import Model as DDIM
     from EMA import EMA
-    
+
     torch.manual_seed(args.seed + rank)
     torch.cuda.manual_seed(args.seed + rank)
     torch.cuda.manual_seed_all(args.seed + rank)
@@ -251,7 +273,12 @@ def train(rank, gpu, args):
                                                drop_last = True)
     
     netG = NCSNpp(args).to(device)
-    
+
+    with open(args.ddim, "r") as f:
+        ddim_config = dict2namespace(yaml.safe_load(f))
+    netTrg = DDIM(ddim_config).to(device)
+    netTrg.load_state_dict(torch.load(ddim_config.model.pretrained))
+
 
     if args.dataset == 'cifar10' or args.dataset == 'stackmnist':    
         netD = Discriminator_small(nc = 2*args.num_channels, ngf = args.ngf,
@@ -262,8 +289,10 @@ def train(rank, gpu, args):
                                    t_emb_dim = args.t_emb_dim,
                                    act=nn.LeakyReLU(0.2)).to(device)
     
+    
     broadcast_params(netG.parameters())
     broadcast_params(netD.parameters())
+    broadcast_params(netTrg.parameters())
     
     optimizerD = optim.Adam(netD.parameters(), lr=args.lr_d, betas = (args.beta1, args.beta2))
     
@@ -275,13 +304,12 @@ def train(rank, gpu, args):
     schedulerG = torch.optim.lr_scheduler.CosineAnnealingLR(optimizerG, args.num_epoch, eta_min=1e-5)
     schedulerD = torch.optim.lr_scheduler.CosineAnnealingLR(optimizerD, args.num_epoch, eta_min=1e-5)
     
-    
-    
     #ddp
     netG = nn.parallel.DistributedDataParallel(netG, device_ids=[gpu])
     netD = nn.parallel.DistributedDataParallel(netD, device_ids=[gpu])
+    netTrg = nn.parallel.DistributedDataParallel(netTrg, device_ids=[gpu])
+    netTrg.eval()
 
-    
     exp = args.exp
     parent_dir = "./saved_info/dd_gan/{}".format(args.dataset)
 
@@ -293,8 +321,7 @@ def train(rank, gpu, args):
             shutil.copytree('score_sde/models', os.path.join(exp_path, 'score_sde/models'))
     
     
-    coeff = Diffusion_Coefficients(args, device)
-    pos_coeff = Posterior_Coefficients(args, device)
+    coeff = Diffusion_Coefficients(ddim_config, device)
     T = get_time_schedule(args, device)
     
     if args.resume:
@@ -334,12 +361,12 @@ def train(rank, gpu, args):
             #sample t
             t = torch.randint(0, args.num_timesteps, (real_data.size(0),), device=device)
             
-            x_t, x_tp1 = q_sample_pairs(coeff, real_data, t)
-            x_t.requires_grad = True
+            x_t_D, x_tp1_D = q_sample_pairs(coeff, real_data, t, netTrg)
+            x_t_D.requires_grad = True
             
     
             # train with real
-            D_real = netD(x_t, t, x_tp1.detach()).view(-1)
+            D_real = netD(x_t_D, t, x_tp1_D).view(-1)
             
             errD_real = F.softplus(-D_real)
             errD_real = errD_real.mean()
@@ -349,7 +376,7 @@ def train(rank, gpu, args):
             
             if args.lazy_reg is None:
                 grad_real = torch.autograd.grad(
-                            outputs=D_real.sum(), inputs=x_t, create_graph=True
+                            outputs=D_real.sum(), inputs=x_t_D, create_graph=True
                             )[0]
                 grad_penalty = (
                                 grad_real.view(grad_real.size(0), -1).norm(2, dim=1) ** 2
@@ -361,7 +388,7 @@ def train(rank, gpu, args):
             else:
                 if global_step % args.lazy_reg == 0:
                     grad_real = torch.autograd.grad(
-                            outputs=D_real.sum(), inputs=x_t, create_graph=True
+                            outputs=D_real.sum(), inputs=x_t_D, create_graph=True
                             )[0]
                     grad_penalty = (
                                 grad_real.view(grad_real.size(0), -1).norm(2, dim=1) ** 2
@@ -374,18 +401,14 @@ def train(rank, gpu, args):
             # train with fake
             latent_z = torch.randn(batch_size, nz, device=device)
             
-         
-            x_0_predict = netG(x_tp1.detach(), t, latent_z)
-            x_pos_sample = sample_posterior(pos_coeff, x_0_predict, x_tp1, t)
+            x_t_D_predict = netG(x_tp1_D, t, latent_z)
             
-            output = netD(x_pos_sample, t, x_tp1.detach()).view(-1)
-                
+            output = netD(x_t_D_predict, t, x_tp1_D).view(-1)
             
             errD_fake = F.softplus(output)
             errD_fake = errD_fake.mean()
             errD_fake.backward()
-    
-            
+
             errD = errD_real + errD_fake
             # Update D
             optimizerD.step()
@@ -400,27 +423,22 @@ def train(rank, gpu, args):
             t = torch.randint(0, args.num_timesteps, (real_data.size(0),), device=device)
             
             
-            x_t, x_tp1 = q_sample_pairs(coeff, real_data, t)
-                
+            x_t_D, x_tp1_D = q_sample_pairs(coeff, real_data, t, netTrg)
             
             latent_z = torch.randn(batch_size, nz,device=device)
             
-            
-                
            
-            x_0_predict = netG(x_tp1.detach(), t, latent_z)
-            x_pos_sample = sample_posterior(pos_coeff, x_0_predict, x_tp1, t)
+            x_t_D_predict = netG(x_tp1_D, t, latent_z)
             
-            output = netD(x_pos_sample, t, x_tp1.detach()).view(-1)
+            output = netD(x_t_D_predict, t, x_tp1_D).view(-1)
                
             
             errG = F.softplus(-output)
             errG = errG.mean()
             
             errG.backward()
+
             optimizerG.step()
-                
-           
             
             global_step += 1
             if iteration % 100 == 0:
@@ -434,10 +452,10 @@ def train(rank, gpu, args):
         
         if rank == 0:
             if epoch % 10 == 0:
-                torchvision.utils.save_image(x_pos_sample, os.path.join(exp_path, 'xpos_epoch_{}.png'.format(epoch)), normalize=True)
+                torchvision.utils.save_image(x_t_D_predict, os.path.join(exp_path, 'xpos_epoch_{}.png'.format(epoch)), normalize=True)
             
             x_t_1 = torch.randn_like(real_data)
-            fake_sample = sample_from_model(pos_coeff, netG, args.num_timesteps, x_t_1, T, args)
+            fake_sample = sample_from_model(coeff, netG, (args.num_timesteps // args.step_size), x_t_1, T, args, netTrg)
             torchvision.utils.save_image(fake_sample, os.path.join(exp_path, 'sample_discrete_epoch_{}.png'.format(epoch)), normalize=True)
             
             if args.save_content:
@@ -535,7 +553,8 @@ if __name__ == '__main__':
     parser.add_argument('--exp', default='experiment_cifar_default', help='name of experiment')
     parser.add_argument('--dataset', default='cifar10', help='name of dataset')
     parser.add_argument('--nz', type=int, default=100)
-    parser.add_argument('--num_timesteps', type=int, default=4)
+    parser.add_argument('--num_timesteps', type=int, default=1000)
+    parser.add_argument('--step_size', type=int, default=166)
 
     parser.add_argument('--z_emb_dim', type=int, default=256)
     parser.add_argument('--t_emb_dim', type=int, default=256)
@@ -558,6 +577,8 @@ if __name__ == '__main__':
     parser.add_argument('--r1_gamma', type=float, default=0.05, help='coef for r1 reg')
     parser.add_argument('--lazy_reg', type=int, default=None,
                         help='lazy regulariation.')
+    parser.add_argument('--ddim', type=str, default=None,
+                        help='Target ddim model config')
 
     parser.add_argument('--save_content', action='store_true',default=False)
     parser.add_argument('--save_content_every', type=int, default=50, help='save content for resuming every x epochs')
@@ -574,6 +595,7 @@ if __name__ == '__main__':
                         help='rank of process in the node')
     parser.add_argument('--master_address', type=str, default='127.0.0.1',
                         help='address for master')
+    
 
    
     args = parser.parse_args()
